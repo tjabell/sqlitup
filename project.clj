033671e80ee281d7d3652c9ec33d4df38fbdfd85
Trevor Abell <trevor.abell@parsus.com>
2017-03-25 08:59:01 -0700

    Add database access code

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/java.jdbc "0.7.0-alpha3"]
+                 [com.microsoft.sqlserver/sqljdbc4 "4.2"]])
--- src/sql_walker/core.clj
+++ src/sql_walker/core.clj
@@ -1,4 +1,25 @@
-(ns sql-walker.core)
+(ns sql-walker.core
+  (:require [clojure.string :as s]
+            [clojure.java.jdbc :as j]
+            [sql-walker.config :as config]))
+
+(def db config/db)
+
+(defn q
+  [query]
+  (j/query db query))
+
+(defn s
+  [t]
+  (q (str "select * from " t)))
+
+(defn ist [t]
+  (def is "INFORMATION_SCHEMA")
+  (str is "." t))
+
+(def tc  (ist "TABLE_CONSTRAINTS"))
+(def ccu (ist "CONSTRAINT_COLUMN_USAGE"))
+(def rc  (ist "REFERENTIAL_CONSTRAINTS"))
 
 (defn foo
   "I don't do a whole lot."
